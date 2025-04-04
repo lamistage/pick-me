63ebbde1a27ae8b059e55a37e0e14823aa889733
nica <lamistage@gmail.com>
2025-04-04 13:44:39 +0700

    feat - dockerize.sh updated (auto increment of version)

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -10,20 +10,71 @@ if [[ -z "${project}" ]]; then
   project=$PROJECT
 fi
 
+current_version=$(cat ${DIR}/version)
+
+while getopts ":Mm" opt; do
+  case $opt in
+    M) increment="major";;
+    m) increment="minor";;
+    \?) echo "Invalid key -$OPTARG" >&2; exit 1;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+clean() {
+  echo "Cleaning ${HUB_USER}/${project}:${current_version}"
+
+  docker rmi ${HUB_USER}/${project}:${current_version}
+}
+
+update_version() {
+  IFS='.' read -ra parts <<< "$current_version"
+  major=${parts[0]}
+  minor=${parts[1]}
+  patch=${parts[2]}
+
+  case $increment in
+    "major")
+      ((major++))
+      minor=0
+      patch=0
+      ;;
+    "minor")
+      ((minor++))
+      patch=0
+      ;;
+    *)
+      ((patch++))
+      ;;
+  esac
+
+  new_version="${major}.${minor}.${patch}"
+  echo $new_version > ${DIR}/version
+  echo $new_version
+}
+
 build() {
+  echo "Building ${HUB_USER}/${project}:${new_version}"
+
+  cd ..
+  mvn clean install
+  cd docker
+
   docker build \
-  -t ${project}:`cat ${DIR}/version` \
+  -t ${HUB_USER}/${project}:${new_version} \
   -f ${DIR}/Dockerfile ${DIR}/..
 }
 
-clean() {
-  docker rmi ${project}:`cat ${DIR}/version`
-}
-
 push() {
+  echo "Pushing ${HUB_USER}/${project}:${new_version}"
+
   docker login -u $HUB_USER -p $HUB_PASSWORD
 
-  docker push ${project}:`cat ${DIR}/version`
+  docker push ${HUB_USER}/${project}:${new_version}
 }
 
-$1
+clean
+new_version=$(update_version)
+build
+push
